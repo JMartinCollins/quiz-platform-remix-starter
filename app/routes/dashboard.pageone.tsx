6a4c488206511ddb61ceb5719f652e7492e8ae8b
JMartinCollins <61546355+JMartinCollins@users.noreply.github.com>
2024-10-24 16:18:42 +0800

    Added dashboard with breadcrumbs

--- app/routes/dashboard.pageone.tsx
+++ app/routes/dashboard.pageone.tsx
@@ -1,3 +1,11 @@
+import { BreadcrumbHandle } from '~/components/layouts/dashboard/dashboardBreadcrumbs';
+
+export const handle: BreadcrumbHandle = {
+    breadcrumb: {
+        "title": "Page One",
+        "path": "/dashboard/pageone"
+    }
+}
 
 export default function DashboardIndex() {
     return <div>Page one</div>;
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,11 +1,26 @@
 import { Outlet } from '@remix-run/react';
+import { BreadcrumbHandle, DashboardBreadcrumbs } from '~/components/layouts/dashboard/dashboardBreadcrumbs';
+
+
+
+export const handle: BreadcrumbHandle = {
+    breadcrumb: {
+        title: "Dashboard",
+        path: "/dashboard"
+    }
+}
 
 export default function Dashboard() {
+
     return <div>
-        <div>{`Here's the layout`}</div>
+        <div>
+            <DashboardBreadcrumbs />
+        </div>
         <div>
             <Outlet />
         </div>
     </div>;
 }
+
+
 
